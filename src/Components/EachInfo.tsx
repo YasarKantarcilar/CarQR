6af49d3c3a758e8f2ef3dcc1154ceb4a10811bc0
YasarKantarcilar <kantarcilaryasar@gmail.com>
2023-11-17 13:56:40 +0300

    FIX: problem of info title overflow

--- src/Components/EachInfo.tsx
+++ src/Components/EachInfo.tsx
@@ -13,7 +13,7 @@ const EachInfo: React.FC<Info> = ({ title, value }) => {
 			initial={{ opacity: 0, x: 200, y: -100 }}
 			animate={{ opacity: 1, x: 0, y: 0 }}
 			transition={{ duration: 1 }}
-			className="w-[90%] min-w-[300px] hover:bg-black duration-500 flex-col sm:flex-col h-[90px] flex justify-center items-center bg-white-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border border-gray-100"
+			className="w-[90%] min-w-[300px] duration-500 flex-col sm:flex-col h-[90px] flex justify-center items-center bg-white-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border border-gray-100"
 		>
 			<p className="text-lg text-white font-mono">{title}</p>
 			<p className="text-md text-center h-1/2 w-full break-words text-white font-mono">{value}</p>
